82058f73d0ada1eb89a6157f3cb5297d7d5648ad
Johnnydiazjr <167589880+Johnnydiazjr@users.noreply.github.com>
2024-05-15 13:05:43 -0400

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-const button1 = document.querySelector("#button1");
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
@@ -21,7 +20,6 @@ const weapons = [
   { name: 'sword', power: 100 }
 ];
 const locations = [
-
   {
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
@@ -33,11 +31,16 @@ const locations = [
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store."
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters."
   }
 ];
 
-// initialize buttons.
-
+// initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
@@ -77,5 +80,30 @@ function buyHealth() {
   } else {
     text.innerText = "You do not have enough gold to buy health.";
   }
+}
+
+function buyWeapon() {
+  if (currentWeapon < weapons.length) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  }
+}
+
+
+function fightSlime() {
+
+}
+
+function fightBeast() {
+
 }
 
